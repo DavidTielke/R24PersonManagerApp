85f224a46784bc5a25d224178b54c027185e8ca1
David Tielke <david.tielke@hotmail.de>
2024-06-25 09:57:58 +0200

    Add project files.

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ConsoleClient
+{
+    class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var persons = File
+                .ReadAllLines("data.csv")
+                .Select(l => l.Split(","))
+                .Select(p => new Person
+                {
+                    Id = int.Parse(p[0]),
+                    Name = p[1],
+                    Age = int.Parse(p[2]),
+                })
+                .ToList();
+
+            var adults = persons.Where(p => p.Age >= 18).ToList();
+            Console.WriteLine($"### Erwachsene {adults.Count} ###");
+            adults.ForEach(a => Console.WriteLine(a.Name));
+            
+            var children = persons.Where(p => p.Age < 18).ToList();
+            Console.WriteLine($"### Kinder {children.Count} ###");
+            children.ForEach(c => Console.WriteLine(c.Name));
+        }
+    }
+}
